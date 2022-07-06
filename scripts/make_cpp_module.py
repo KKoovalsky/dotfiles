10f3b353b34459fac734cb2a8b3c8e1807c29336
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-07-06 11:24:05 +0200

    Support interface modules

--- scripts/make_cpp_module.py
+++ scripts/make_cpp_module.py
@@ -14,24 +14,26 @@ from os import path
               default='static_lib')
 @click.option('--brief_in_cpp', type=str,
               help='Mandatory if --module_type is static_lib')
-@click.option('--public_dependencies', type=str,
+@click.option('--public_deps', type=str,
               help='CMake targets linked to the created library as PUBLIC')
 def make_cpp_module(module_name,
                     brief_description_in_header,
                     module_type,
                     brief_in_cpp,
-                    public_dependencies):
+                    public_deps):
     if module_type == "static_lib" and not brief_in_cpp:
         raise RuntimeError(
             'Pass --brief_in_cpp as a brief description within the source file'
         )
-    if module_type == 'interface':
-        raise RuntimeError('Module type = interface not supported yet')
 
     os.mkdir(module_name)
     generate_header_file(module_name, brief_description_in_header)
-    generate_source_file(module_name, brief_in_cpp)
-    generate_cmake_file_for_static_lib(module_name, public_dependencies)
+
+    if module_type == "static_lib":
+        generate_source_file(module_name, brief_in_cpp)
+        generate_cmake_file_for_static_lib(module_name, public_deps)
+    else:
+        generate_cmake_file_for_interface_lib(module_name, public_deps)
 
 
 def generate_header_file(module_name: str, brief_description_in_header: str):
@@ -67,7 +69,7 @@ def generate_source_file(module_name: str, brief_in_cpp: str):
 
 
 def generate_cmake_file_for_static_lib(module_name: str,
-                                       public_dependencies: str):
+                                       public_deps: str):
     source_filename = '{}.cpp'.format(module_name)
     content = (
         'add_library({0} STATIC {1})\n'
@@ -75,9 +77,26 @@ def generate_cmake_file_for_static_lib(module_name: str,
         'PUBLIC ${{CMAKE_CURRENT_LIST_DIR}})\n').format(
             module_name, source_filename)
 
-    if public_dependencies:
+    if public_deps:
         content += 'target_link_libraries({0} PUBLIC {1})'.format(
-            module_name, public_dependencies)
+            module_name, public_deps)
+
+    p = path.join(module_name, 'CMakeLists.txt')
+    with open(p, 'w') as f:
+        f.write(content)
+
+
+def generate_cmake_file_for_interface_lib(module_name: str,
+                                          public_deps: str):
+
+    content = (
+        'add_library({0} INTERFACE)\n'
+        'target_include_directories({0} '
+        'INTERFACE ${{CMAKE_CURRENT_LIST_DIR}})\n').format(module_name)
+
+    if public_deps:
+        content += 'target_link_libraries({0} INTERFACE {1})'.format(
+            module_name, public_deps)
 
     p = path.join(module_name, 'CMakeLists.txt')
     with open(p, 'w') as f:
