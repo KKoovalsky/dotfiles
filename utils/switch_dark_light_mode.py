e9127a02ff1836eb4e634ff083119cb747beafb8
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2023-02-06 12:20:05 +0100

    Extend dark/light mode switcher by changing the terminator border

--- utils/switch_dark_light_mode.py
+++ utils/switch_dark_light_mode.py
@@ -5,13 +5,17 @@ import os
 import re
 import click
 
-theme = {"dark": "Dracula", "light": "Github"}
+theme = {
+    "dark": ("Dracula", "rgb(200, 200, 200)"),
+    "light": ("Github", "rgb(20, 20, 20)"),
+}
 
 
 @click.command()
 @click.argument("mode", type=click.Choice(["dark", "light"]))
 def main(mode):
     set_terminator_config(mode)
+    set_terminator_border_color(mode)
 
 
 def set_terminator_config(mode):
@@ -25,9 +29,22 @@ def set_terminator_config(mode):
         r" window0\s+profile ="
         r" )(\w+)"
     )
-    theme_name = theme[mode]
+    theme_name = theme[mode][0]
     res = re.sub(regex, r"\1{}".format(theme_name), config)
     with open(path, "w") as f:
+        f.write(res)
+
+
+def set_terminator_border_color(mode):
+    gtk_css_path = os.path.join(os.path.dirname(__file__), "..", "gtk.css")
+    regex = re.compile(
+        r"(:root {\s+" r"--terminator-separator-color: )(.*)([\n\r])"
+    )
+    with open(gtk_css_path) as f:
+        gtk_css = f.read()
+    border_color = theme[mode][1]
+    res = re.sub(regex, r"\1{}\3".format(border_color), gtk_css)
+    with open(gtk_css_path, "w") as f:
         f.write(res)
 
 
